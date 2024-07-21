b9545e0c015bd635f12035da6e76587fe5a94112
joshdoom <156129144+joshdoom@users.noreply.github.com>
2024-07-21 17:47:54 -0400

    Add files via upload

--- Proyecto-tienda/checkout.php
+++ Proyecto-tienda/checkout.php
@@ -11,7 +11,7 @@ print_r($_SESSION);
 $lista_carrito = array();
 
 if ($producto != null) {
-  foreach ($productos as $clave => $cantidad) {
+  foreach ($producto as $clave => $cantidad) {
     $sql = $con->prepare("SELECT id, nombre, precio, $cantidad AS cantidad FROM producto WHERE id=? AND activo=1");
     $sql->execute([$clave]);
     $lista_carrito[] = $sql->fetch(PDO::FETCH_ASSOC);
@@ -77,7 +77,7 @@ if ($producto != null) {
             </thead>
             <tbody>
               <tr>
-                <?php if ($lista_carrito = null)  {
+                <?php if ($lista_carrito == null) {
                   echo '<tr><td colspan="5" class="text-center"><b>lista vacia</b></td></tr>';
                 } else {
                   $total = 0;
@@ -93,21 +93,20 @@ if ($producto != null) {
                 <td><?php echo $nombre; ?></td>
                 <td><?php echo MONEDA . number_format($precio, 2, '.', ','); ?></td>
                 <td>
-                  <input type="number" min="1" max="10" step="1" value="<?php echo $cantidad ?>" size="5" id="cantidad_<?php echo $_id; ?>" 
-                  onchange="actualizaCantidad(this.value,<?php echo $_id; ?>)">
+                  <input type="number" min="1" max="10" step="1" value="<?php echo $cantidad ?>" size="5" id="cantidad_<?php echo $_id; ?>" onchange="actualizaCantidad(this.value,<?php echo $_id; ?>)">
                 </td>
                 <td>
-                  <div id="subtotal_<?php echo $_id; ?>" name="subtotal[]"<?php echo MONEDA . number_format($subtotal, 2, '.', ','); ?>"></div>
+                  <div id="subtotal_<?php echo $_id; ?>" name="subtotal[]" <?php echo MONEDA . number_format($subtotal, 2, '.', ','); ?>"></div>
                 </td>
                 <td>
-                  <a href="&" id="eliminar" class="btn btn-warning btn-sm" data-bs-id="<?php echo $_id; ?>" data-bs-toggle="modal" data-bs-target="eliminaModal">Eliminar</a>
+                  <a href="&" id="eliminar" class="btn btn-warning btn-sm" data-bs-id="<?php echo $_id; ?>" data-bs-toggle="modal" data-bs-target="#eliminaModal">Eliminar</a>
                 </td>
               </tr>
             <?php } ?>
             <tr>
               <td colspan="3"></td>
               <td colspan="2">
-                <p class="h3" id="total"><?php echo MONEDA. number_format($total, 2, '.', ',');?></p>
+                <p class="h3" id="total"><?php echo MONEDA . number_format($total, 2, '.', ','); ?></p>
               </td>
             </tr>
             </tbody>
@@ -125,16 +124,44 @@ if ($producto != null) {
       </div>
     </main>
 
+    <!-- Modal -->
+    <div class="modal fade" id="eliminaModal" tabindex="-1" aria-labelledby="eliminaModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-ms">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="eliminaModalLabel">Alerta</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ¿Desea eliminar el producto del carrito?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button id="btn-elimina" type="button" class="btn btn-danger" onclick="elimina()">Eliminar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
   </header>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
   <script>
+
+    let eliminaModal = document.getElementById('eliminaModal')
+    eliminaModal.addEventListener('show.bs.modal', function(event){
+      let button = event.relatedTarget
+      let id =button.getAttribute('data-bs-id')
+      let buttonElimina = eliminaModal.querySelector('.moda-footer #btn-elinima')
+      buttonElimina.value = id
+    })
+
     function actualizaCantidad(cantidad, id) {
       let url = 'clases/actualizar_carrito,php'
       let formdata = new FormData()
-      formdata.append( 'action','agregar')
+      formdata.append('action', 'agregar')
       formdata.append('id', id)
       formdata.append('cantidad', cantidad)
 
@@ -151,13 +178,13 @@ if ($producto != null) {
           divsubtotal.innerHTML = data.sub
 
           let total = 0.00
-          let list= document.getElementsByName('subtotal[]')
+          let list = document.getElementsByName('subtotal[]')
 
-          for(let i = 0; i < list.length; i ++){
+          for (let i = 0; i < list.length; i++) {
             total += parseFloat(list[i].innerHTML.replace(/[$,]/g, ''))
           }
 
-          total = new Intl.NumberFormat('en-US',{
+          total = new Intl.NumberFormat('en-US', {
             minimumFractionDigits: 2
           }).format(total)
           document.getElementById('total').innerHTML = '<?php echo MONEDA ?>' + total
